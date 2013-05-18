1a485204881d0bc37aca6c41ac37e2ce7844b0d8
Lena Hsieh <lena_hsieh@hotmail.com>
2013-05-18 00:44:46 -0400

    playing with panels

--- src/MinesweeperFrame.java
+++ src/MinesweeperFrame.java
@@ -1,15 +1,22 @@
 import javax.swing.*;
+import javax.swing.plaf.PanelUI;
+import javax.swing.plaf.basic.DefaultMenuLayout;
 import java.awt.*;
 
-/**
- * User: Lena
- * Date: 2013-05-02
- */
 public class MinesweeperFrame extends JFrame{
 
     public MinesweeperFrame(){
 
-        setLayout(new GridLayout(9,9, 0, 0));
+//        setLayout(new GridLayout(9, 9, 0, 0));
+
+        JPanel panel1 = new JPanel();
+        panel1.setLayout(new BoxLayout(panel1, BoxLayout.LINE_AXIS));
+        panel1.add(new JButton("hello"));
+        panel1.add(new JButton("again"));
+        add(panel1);
+        JLabel label1 = new JLabel();
+        label1.setText("start playing");
+        add(label1);
 
         add(new JButton("0"));
         add(new JButton("1"));
@@ -52,46 +59,46 @@ public class MinesweeperFrame extends JFrame{
         add(new JButton("38"));
         add(new JButton("39"));
         add(new JButton("40"));
-        add(new JButton("41"));
-        add(new JButton("42"));
-        add(new JButton("43"));
-        add(new JButton("44"));
-        add(new JButton("45"));
-        add(new JButton("46"));
-        add(new JButton("47"));
-        add(new JButton("48"));
-        add(new JButton("49"));
-        add(new JButton("50"));
-        add(new JButton("51"));
-        add(new JButton("52"));
-        add(new JButton("53"));
-        add(new JButton("54"));
-        add(new JButton("55"));
-        add(new JButton("56"));
-        add(new JButton("57"));
-        add(new JButton("58"));
-        add(new JButton("59"));
-        add(new JButton("60"));
-        add(new JButton("61"));
-        add(new JButton("62"));
-        add(new JButton("63"));
-        add(new JButton("64"));
-        add(new JButton("65"));
-        add(new JButton("66"));
-        add(new JButton("67"));
-        add(new JButton("68"));
-        add(new JButton("69"));
-        add(new JButton("70"));
-        add(new JButton("71"));
-        add(new JButton("72"));
-        add(new JButton("73"));
-        add(new JButton("74"));
-        add(new JButton("75"));
-        add(new JButton("76"));
-        add(new JButton("77"));
-        add(new JButton("78"));
-        add(new JButton("79"));
-        add(new JButton("80"));
+//        add(new JButton("41"));
+//        add(new JButton("42"));
+//        add(new JButton("43"));
+//        add(new JButton("44"));
+//        add(new JButton("45"));
+//        add(new JButton("46"));
+//        add(new JButton("47"));
+//        add(new JButton("48"));
+//        add(new JButton("49"));
+//        add(new JButton("50"));
+//        add(new JButton("51"));
+//        add(new JButton("52"));
+//        add(new JButton("53"));
+//        add(new JButton("54"));
+//        add(new JButton("55"));
+//        add(new JButton("56"));
+//        add(new JButton("57"));
+//        add(new JButton("58"));
+//        add(new JButton("59"));
+//        add(new JButton("60"));
+//        add(new JButton("61"));
+//        add(new JButton("62"));
+//        add(new JButton("63"));
+//        add(new JButton("64"));
+//        add(new JButton("65"));
+//        add(new JButton("66"));
+//        add(new JButton("67"));
+//        add(new JButton("68"));
+//        add(new JButton("69"));
+//        add(new JButton("70"));
+//        add(new JButton("71"));
+//        add(new JButton("72"));
+//        add(new JButton("73"));
+//        add(new JButton("74"));
+//        add(new JButton("75"));
+//        add(new JButton("76"));
+//        add(new JButton("77"));
+//        add(new JButton("78"));
+//        add(new JButton("79"));
+//        add(new JButton("80"));
 
 
 
